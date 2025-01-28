47ab79ab526129968ba9b2da578223b999d951ad
mounika-yenumula <mounikamadhav24@gmail.com>
2025-01-28 13:52:56 -0500

    second commit

--- src/Components/NewTask.jsx
+++ src/Components/NewTask.jsx
@@ -6,6 +6,10 @@ export default function NewTask({onAdd}){
         setEnteredTask(event.target.value);
     }
     function handleClick(){
+        if(enteredTask.trim() === ''){
+                return;
+        }
+        
         onAdd(enteredTask);
         setEnteredTask('');
 
